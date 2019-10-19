d3b2614140623c1eb178f5c6d0e93773b88855c1
Wilscos <43081060+Wilscos@users.noreply.github.com>
2019-10-19 10:37:45 +0200

    Added file via upload

---
+++ Randomized Polygon Ninja Turtles.py
@@ -0,0 +1,27 @@
+import turtle
+import random
+
+wn = turtle.Screen()
+wn.bgcolor("lightyellow")
+wn.colormode(255)
+alex = turtle.Turtle()
+alex.shape("turtle")
+
+
+def polygons(t, sides):
+    for side in range(sides):
+        t.color(random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
+        t.stamp()
+        t.forward(30)
+        t.left(180 - (180 * (sides - 2) // sides))
+
+
+polygons_sides = [3, 4, 6, 8]
+for i in range(30):
+    alex.up()
+    alex.setpos(float(random.randrange(-200, 200)), float(random.randrange(-200, 200)))
+    alex.down()
+    alex.pensize(random.randrange(2, 10))
+    polygons(alex, random.choice(polygons_sides))
+
+wn.exitonclick()
